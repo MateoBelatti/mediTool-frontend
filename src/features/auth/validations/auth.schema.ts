c10dd4015308ffbac4a0f57a692db8994625e9f6
MateoBelatti <mateobelatti7@gmail.com>
2026-07-13 12:22:54 -0300

    schema para validacion

---
+++ src/features/auth/validations/auth.schema.ts
@@ -0,0 +1,15 @@
+import { z } from 'zod'
+
+export const loginSchema = z.object({
+  email: z
+    .string()
+    .min(1, 'El email es requerido')
+    .email('Formato de email inválido'),
+  password: z.string().min(1, 'La contraseña es requerida'),
+})
+
+export type LoginFormData = z.infer<typeof loginSchema>
+
+export const refreshTokenSchema = z.object({
+  refreshToken: z.string().min(1, 'El refresh token es requerido'),
+})
