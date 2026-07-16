5122c44fe0f7d6db3994d7b8aa288d0c335f1c0f
MateoBelatti <mateobelatti7@gmail.com>
2026-07-16 14:47:11 -0300

    actualizacion de importacion de page

--- src/app/router/AppRouter.tsx
+++ src/app/router/AppRouter.tsx
@@ -2,7 +2,7 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { MainLayout } from '@/app/layouts/MainLayout'
 import { HomePage } from '@/features/dashboard/pages/HomePage'
 import { LoginPage } from '@/dashboard/pages/LoginPage/LoginPage'
-import { PacientesPage } from '@/features/pacientes/pages/PacientesPage'
+import { PacientesPage } from '@/dashboard/pages/pacientesPage/PacientesPage'
 
 const router = createBrowserRouter([
   {
